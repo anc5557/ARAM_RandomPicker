f7ba1631d585159b3a06b6bb14313b6cb2b1f043
junho <anc5557@naver.com>
2023-11-13 16:55:26 +0900

    Add navigation bar to app.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import Link from 'next/link'
 import 'tailwindcss/tailwind.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
@@ -12,6 +13,13 @@ const App = ({ Component, pageProps }: AppProps) => {
       <Head>
         <title>ARAM Auto Random Pick</title>
       </Head>
+      <nav className="flex items-center justify-between flex-wrap bg-purple-500 p-6">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <Link href="/">
+            <span className="font-semibold text-xl tracking-tight ">ARAM Auto Random Pick</span>
+          </Link>
+        </div>
+      </nav>
       <Component {...pageProps} />
     </>
   )
