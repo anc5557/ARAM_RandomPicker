6b2ce2c414889aa28c8ef2be131d30f26c51530d
junho <anc5557@naver.com>
2023-11-13 17:20:10 +0900

    Fix team count input styling and spacing in Settings page

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -17,27 +17,36 @@ export default function Settings() {
     };
 
     return (
-        <div className="container mx-auto px-4 py-12">
+        <div className="container mx-auto px-4 py-12 pb-10"> 
             <h1 className="text-4xl font-bold text-center mb-16">Setting</h1>
-            <p className="text-center text-lg  mb-10">
-                Randomly pick champions for each team based on the number of team members and the number of dice. 
+            <p className="text-center text-lg  mb-5">
+                Randomly pick champions for each team based on the number of team members and the number of dice.
             </p>
             <p className="text-center text-lg  mb-20">
-            Number of champions on each team = (number of team members + 1) * dice number
+                Number of champions on each team = (number of team members + 1) * dice number
             </p>
             <div className="flex justify-center space-x-6">
                 {['dice', 'redTeam', 'blueTeam'].map((team, index) => (
                     <div className="w-1/5" key={index}>
                         <label htmlFor={`${team}Count`} className="block text-sm font-medium text-gray-700">
-                            {team === 'dice' ? 'Number of dice' : team === 'redTeam' ? <span style={{color: 'red'}}>Red Team Count</span> : <span style={{color: 'blue'}}>Blue Team Count</span>}
+                            {team === 'dice' ? 'Number of dice' : team === 'redTeam' ? <span style={{ color: 'red' }}>Red Team Count</span> : <span style={{ color: 'blue' }}>Blue Team Count</span>}
                         </label>
                         <input
                             type="number"
                             id={`${team}Count`}
-                            className={`mt-1 form-input block w-full px-2 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-${team === 'redTeam' ? 'red' : team === 'blueTeam' ? 'blue' : 'indigo'}-500 focus:border-${team === 'redTeam' ? 'red' : team === 'blueTeam' ? 'blue' : 'indigo'}-500`}
+                            className={`mt-1 form-input block w-full px-2 py-1 border ${team === 'redTeam' ? 'border-red-500 focus:ring-red-500 focus:border-red-500' :
+                                    team === 'blueTeam' ? 'border-blue-500 focus:ring-blue-500 focus:border-blue-500' :
+                                        'border-black focus:ring-indigo-500 focus:border-indigo-500' 
+                                }`}
                             value={team === 'dice' ? diceCount : team === 'redTeam' ? redTeamCount : blueTeamCount}
-                            onChange={(e) => team === 'dice' ? setDiceCount(Number(e.target.value)) : team === 'redTeam' ? setRedTeamCount(Number(e.target.value)) : setBlueTeamCount(Number(e.target.value))}
-                            min={team === 'dice' ? 0 : 1} max={team === 'dice' ? 2 : 5}
+                            onChange={(e) => {
+                                const value = Number(e.target.value);
+                                if (team === 'dice') setDiceCount(value);
+                                else if (team === 'redTeam') setRedTeamCount(value);
+                                else setBlueTeamCount(value);
+                            }}
+                            min={team === 'dice' ? 0 : 1}
+                            max={team === 'dice' ? 2 : 5}
                         />
                     </div>
                 ))}
